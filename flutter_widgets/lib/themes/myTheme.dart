03313ec763fd93ffe1888aeb2533c0b0321e694b
Jenny <jeniperkuki@gmail.com>
2026-06-11 08:03:19 +0300

    init: project reinitalization

--- flutter_widgets/lib/themes/myTheme.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Mytheme {
-  //convert to private class
-  Mytheme._();
-
-  static ThemeData lightTheme = ThemeData.light();
-  static ThemeData darkTheme = ThemeData.dark();
-}
