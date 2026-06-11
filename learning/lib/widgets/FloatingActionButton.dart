208b287c99f8eb13a3475f020819aa6e26ac4d55
Jenny <jeniperkuki@gmail.com>
2026-06-11 16:11:03 +0300

    feat: latest updates

--- learning/lib/widgets/FloatingActionButton.dart
+++ learning/lib/widgets/FloatingActionButton.dart
@@ -6,7 +6,9 @@ class FloatingAction extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
-      onPressed: () {},
+      onPressed: () {
+        Navigator.pushNamed(context, '/');
+      },
       backgroundColor: Color.fromARGB(255, 162, 37, 28),
       foregroundColor: Colors.white,
       child: Icon(Icons.one_k_plus),
